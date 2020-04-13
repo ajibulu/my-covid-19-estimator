b0708a02c4ce3327b722d656e7b93f8eb9c5b9ef
Yemisi Abolanle Ajibulu <48490614+ajibulu@users.noreply.github.com>
2020-04-12 17:12:18 -0700

    updated changes

--- estimator.js
+++ estimator.js
@@ -1,11 +1,18 @@
-
 const covid19ImpactEstimator = (data) => data
 const input = {
+impact: {
+  currentlyInfected:45,
+  infectionsByRequestedTime:434
+},
 region: {
   name: 'Africa',
   avgAge: 19.7,
   avgDailyIncomeInUSD: 5,
   avgDailyIncomePopulation: 0.71,
+},
+severeImpact: {
+  currentlyInfected:45,
+  infectionsByRequestedTime:344
 },
 periodType: 'days',
 population: 58,
@@ -13,9 +20,11 @@ reportedCases: 674,
 timeToElapse:  66622705,
 totalHospitalBeds: 1380614
 };
-//********************Computation of Estimate
- const elapsedTime,factors,twoPowerFactor,availableBeds,casesForICUByRequestedTime,casesForVentilatorsByRequestedTime,dollarsInFlight; 
+
+  //********************Computation of Estimate
+ const elapsedTime,factors,twoPowerFactor,availableBeds,casesForICUByRequestedTime,casesForVentilatorsByRequestedTime;dollarsInFlight; 
  elapsedTime=input.timeToElapse;
+
  const period =() => {
    switch(input.periodType) {
     case 'days':
@@ -53,10 +62,15 @@ totalHospitalBeds: 1380614
      impactInfectionsByRequestedTime=impactCurrentlyInfected * factor();
      severeCurrentlyInfected=input.reportedCases*50;
      SevereInfectionsByRequestedTime=severeCurrentlyInfected*factor();
+   }
+
  };
  
+ 
  twoPowerFactor=Math.pow(2,factors);
-  //Challenge 2
+ 
+
+ //Challenge 2
  severeCasesByRequestedTime=input.severeImpact.infectionsByRequestedTime*0.15;
  availableBeds=input.totalHospitalBeds*0.35;
  hospitalBedsByRequestedTime=availableBeds-severeCasesByRequestedTime;
